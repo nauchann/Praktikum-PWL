5d30afa4bfd70dccc381313fd7ba6d7c7c400f67
nauchann <152774041+nauchann@users.noreply.github.com>
2024-04-28 21:24:20 +0700

    migration and seeder

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.master')
 
 @section('container')
-
+    <h1 class="text-center">Data Dosen</h1>
+    <div class="mb-4">
+    <button type="button" class="btn btn-success ">Tambah Data</button>
+    </div>
+    <div class="row">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nama</th>
+                <th scope="col">NIP</th>
+                <th scope="col">alamat</th>
+                <th scope="col">KTM</th>
+                <th scope="col">Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($dosen as $dsn)
+              <tr>
+                <th scope="row">{{$dsn->id_dosen}}</th>
+                <td>{{$dsn->nama}}</td>
+                <td>{{$dsn->NIP}}</td>
+                <td>{{$dsn->alamat}}</td>
+                <td>{{$dsn->ktm->nomor_identitas}}</th>
+                <td>
+                  <button type="button" class="btn btn-primary">Update</button>
+                  <button type="button" class="btn btn-danger">Hapus</button>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+    </div>
 @endsection
