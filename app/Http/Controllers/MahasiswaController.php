e760dadda586aebef61043c562baa15f8632b4eb
nauchann <152774041+nauchann@users.noreply.github.com>
2024-04-28 21:25:15 +0700

    migration and seeder

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -12,10 +12,11 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        $mahasiswa = Mahasiswa::all(); 
-        return view ('mahasiswa', [
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa', [
             'mahasiswa' => $mahasiswa
         ]);
+
     }
 
     /**
@@ -23,7 +24,7 @@ class MahasiswaController extends Controller
      */
     public function create()
     {
-        //
+        return view('create');
     }
 
     /**
@@ -31,7 +32,19 @@ class MahasiswaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nama' => 'required',
+            'nim' => 'required|unique:mahasiswa',
+            'jurusan' => 'required',
+            'alamat' => 'required',
+        ]);
+        $mahasiswa = new Mahasiswa;
+        $mahasiswa->nama = $request->nama;
+        $mahasiswa->nim = $request->nim;
+        $mahasiswa->jurusan = $request->jurusan;
+        $mahasiswa->alamat = $request->alamat;
+        $mahasiswa->save();
+        return redirect()->route('index');
     }
 
     /**
@@ -45,17 +58,32 @@ class MahasiswaController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id_mahasiswa)
     {
-        //
+        $mahasiswa = mahasiswa::find($id_mahasiswa);
+        return view('update', [
+            'mahasiswa' => $mahasiswa
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id_mahasiswa)
     {
-        //
+        $request->validate([
+            'nama' => 'required',
+            'nim' => 'required|unique:mahasiswa',
+            'jurusan' => 'required',
+            'alamat' => 'required',
+        ]);
+        $mahasiswa = mahasiswa::find($id_mahasiswa);
+        $mahasiswa->nama = $request->nama;
+        $mahasiswa->nim = $request->nim;
+        $mahasiswa->jurusan = $request->jurusan;
+        $mahasiswa->alamat = $request->alamat;
+        $mahasiswa->save();
+        return redirect()->route('index');
     }
 
     /**
@@ -63,6 +91,6 @@ class MahasiswaController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        
     }
 }
